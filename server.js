c8122c16b7297429d8f46e605472830a5fc9db1e
Erin Mahoney <erinmahoneyphoto@gmail.com>
2017-08-18 14:04:40 -0700

    npm init proj

---
+++ server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+
+// Allows app to use external js files
+app.use(express.static('public'));
+
+app.listen(3000);
+
+// root route (serves index.html)
+app.get('/', function(req, res) {
+	res.sendFile(__dirname + '/public/views/index.html');
+});
